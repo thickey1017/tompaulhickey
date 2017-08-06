67b80db8df8b0a3bc93e80968df9be3f2dfe11a6
thickey1017 <30778562+thickey1017@users.noreply.github.com>
2017-08-06 18:37:01 -0500

    Stars redesign
    Total overhaul of site

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,10 +1,47 @@
 function myFunction() {
    document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
-function expSelection() {
-	$(document).ready(function(){
-               $('#expDetail').load('content/education.html');
-           });
+function expSelection(clicked_id) {
+				if (clicked_id=="epic") {
+					document.getElementById("expDetail").innerHTML = "Epic text";
+				} else if (clicked_id=="education") {
+					document.getElementById("expDetail").innerHTML = "Education text";
+				} else if (clicked_id=="amazon"){
+					document.getElementById("expDetail").innerHTML = "Amazon text";
+				}
+}
+function showSection(clicked_id) {
+				if (clicked_id=="aboutMeLink") {
+					$("#aboutMe").removeClass("dispNone");
+					$("#rehome").removeClass("dispNone");
+					$("#patEng").addClass("dispNone");
+					$("#profExp").addClass("dispNone");
+					$("#favReads").addClass("dispNone");
+				} else if (clicked_id=="profExpLink") {
+					$("#profExp").removeClass("dispNone");
+					$("#rehome").removeClass("dispNone");
+					$("#patEng").addClass("dispNone");
+					$("#aboutMe").addClass("dispNone");
+					$("#favReads").addClass("dispNone");
+				} else if (clicked_id=="favReadsLink"){
+					$("#favReads").removeClass("dispNone");
+					$("#rehome").removeClass("dispNone");
+					$("#patEng").addClass("dispNone");
+					$("#profExp").addClass("dispNone");
+					$("#aboutMe").addClass("dispNone");
+				} else if (clicked_id=="patEngLink"){
+					$("#patEng").removeClass("dispNone");
+					$("#rehome").removeClass("dispNone");
+					$("#favReads").addClass("dispNone");
+					$("#profExp").addClass("dispNone");
+					$("#aboutMe").addClass("dispNone");
+				} else if (clicked_id=="rehome"){
+					$("#patEng").addClass("dispNone");
+					$("#rehome").addClass("dispNone");
+					$("#favReads").addClass("dispNone");
+					$("#profExp").addClass("dispNone");
+					$("#aboutMe").addClass("dispNone");
+				}
 }
 //onclick event example- whenever a .tph class is clicked, action happens
 //$(".tph").click(function(){
