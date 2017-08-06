b232e45b8c666086f784491dcb9ec0808d1189aa
thickey1017 <30778562+thickey1017@users.noreply.github.com>
2017-08-06 12:36:23 -0500

    Initial commit
    Initial commit; upload of all 'custom' directory

---
+++ scripts/script1.js
@@ -0,0 +1,12 @@
+function myFunction() {
+   document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+function expSelection() {
+	$(document).ready(function(){
+               $('#expDetail').load('content/education.html');
+           });
+}
+//onclick event example- whenever a .tph class is clicked, action happens
+//$(".tph").click(function(){
+  // action goes here!!
+//});
