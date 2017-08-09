e278d8d3bee64f7dfc84ceb060a4b1cdd5e8ed11
thickey1017 <30778562+thickey1017@users.noreply.github.com>
2017-08-08 20:59:15 -0500

    Complete structure for favorite books
    -Book selection and reset
    -Table structure added
    -Partial back button

--- scripts/script1.js
+++ scripts/script1.js
@@ -5,29 +5,49 @@ function showSection(clicked_id) {
 					$("#patEng").addClass("dispNone");
 					$("#profExp").addClass("dispNone");
 					$("#favReads").addClass("dispNone");
+					resetBooks();
 				} else if (clicked_id=="profExpLink") {
 					$("#profExp").removeClass("dispNone");
 					$("#rehome").removeClass("dispNone");
 					$("#patEng").addClass("dispNone");
 					$("#aboutMe").addClass("dispNone");
 					$("#favReads").addClass("dispNone");
+					resetBooks();
 				} else if (clicked_id=="favReadsLink"){
 					$("#favReads").removeClass("dispNone");
 					$("#rehome").removeClass("dispNone");
 					$("#patEng").addClass("dispNone");
 					$("#profExp").addClass("dispNone");
 					$("#aboutMe").addClass("dispNone");
+					resetBooks();
 				} else if (clicked_id=="patEngLink"){
 					$("#patEng").removeClass("dispNone");
 					$("#rehome").removeClass("dispNone");
 					$("#favReads").addClass("dispNone");
 					$("#profExp").addClass("dispNone");
 					$("#aboutMe").addClass("dispNone");
+					resetBooks();
 				} else if (clicked_id=="rehome"){
 					$("#patEng").addClass("dispNone");
 					$("#rehome").addClass("dispNone");
 					$("#favReads").addClass("dispNone");
 					$("#profExp").addClass("dispNone");
 					$("#aboutMe").addClass("dispNone");
+					resetBooks();
 				}
+}
+function showReadSection(clicked_id) {
+				$("#readOptions").addClass("dispNone");
+				if (clicked_id=="deepImg") {$("#deepDetails").removeClass("dispNone");
+				} else if (clicked_id=="ggsImg") {$("#ggsDetails").removeClass("dispNone");
+				} else if (clicked_id=="driveImg") {$("#driveDetails").removeClass("dispNone");
+				} else if (clicked_id=="fountainImg") {$("#fountainDetails").removeClass("dispNone");
+				}
+}
+function resetBooks(){
+				$("#readOptions").removeClass("dispNone");
+				$("#ggsDetails").addClass("dispNone");
+				$("#driveDetails").addClass("dispNone");
+				$("#deepDetails").addClass("dispNone");
+				$("#fountainDetails").addClass("dispNone");
 }
