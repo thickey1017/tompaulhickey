ef5e00c9f026d5aa42abb2b631676b579b1d6ee9
thickey1017 <30778562+thickey1017@users.noreply.github.com>
2017-08-09 21:47:02 -0500

    Pat Eng Structure
    -Creation of patient engagement structure
    - Wrapping up large structural changes before v1 of content

--- scripts/script1.js
+++ scripts/script1.js
@@ -6,6 +6,7 @@ function showSection(clicked_id) {
 					$("#profExp").addClass("dispNone");
 					$("#favReads").addClass("dispNone");
 					resetBooks();
+					resetPatEng();
 				} else if (clicked_id=="profExpLink") {
 					$("#profExp").removeClass("dispNone");
 					$("#rehome").removeClass("dispNone");
@@ -13,6 +14,7 @@ function showSection(clicked_id) {
 					$("#aboutMe").addClass("dispNone");
 					$("#favReads").addClass("dispNone");
 					resetBooks();
+					resetPatEng();
 				} else if (clicked_id=="favReadsLink"){
 					$("#favReads").removeClass("dispNone");
 					$("#rehome").removeClass("dispNone");
@@ -20,6 +22,7 @@ function showSection(clicked_id) {
 					$("#profExp").addClass("dispNone");
 					$("#aboutMe").addClass("dispNone");
 					resetBooks();
+					resetPatEng();
 				} else if (clicked_id=="patEngLink"){
 					$("#patEng").removeClass("dispNone");
 					$("#rehome").removeClass("dispNone");
@@ -27,6 +30,7 @@ function showSection(clicked_id) {
 					$("#profExp").addClass("dispNone");
 					$("#aboutMe").addClass("dispNone");
 					resetBooks();
+					resetPatEng();
 				} else if (clicked_id=="rehome"){
 					$("#patEng").addClass("dispNone");
 					$("#rehome").addClass("dispNone");
@@ -34,6 +38,7 @@ function showSection(clicked_id) {
 					$("#profExp").addClass("dispNone");
 					$("#aboutMe").addClass("dispNone");
 					resetBooks();
+					resetPatEng();
 				}
 }
 function showReadSection(clicked_id) {
@@ -50,4 +55,17 @@ function resetBooks(){
 				$("#driveDetails").addClass("dispNone");
 				$("#deepDetails").addClass("dispNone");
 				$("#fountainDetails").addClass("dispNone");
+}
+function showPatEngSection(clicked_id) {
+				$("#patEngMenu").addClass("dispNone");
+				if (clicked_id=="patEngMULink") {$("#patEngMU").removeClass("dispNone");
+				} else if (clicked_id=="patEngExpLink") {$("#patEngExp").removeClass("dispNone");
+				} else if (clicked_id=="patEngStrategyLink") {$("#patEngStrategy").removeClass("dispNone");
+				}
+}
+function resetPatEng(){
+				$("#patEngMenu").removeClass("dispNone");
+				$("#patEngMU").addClass("dispNone");
+				$("#patEngExp").addClass("dispNone");
+				$("#patEngStrategy").addClass("dispNone");
 }
